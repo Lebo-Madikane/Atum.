de1a0c90f445707ed7ead035331fb158012c1135
Lebo Madikane <madikanelebohang@gmail.com>
2026-04-21 19:16:49 +0200

    create container structures on all components

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import styles from './Footer.module.scss'
+
+export default function Footer() {
+
+    return (
+        <header className={styles.footer}>
+            <div className={styles.container}>
+
+            </div>
+        </header>
+    )
+}
