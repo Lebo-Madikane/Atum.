9351790303edcd371dbf090fed8657ebc0fec346
Lebo Madikane <madikanelebohang@gmail.com>
2026-05-12 14:26:47 +0200

    build About component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,9 +4,13 @@ import styles from './About.module.scss'
 export default function About() {
 
     return (
-        <header className={styles.about}>
-            <div className={styles.container}>
-
+        <header className={styles.aboutSection}>
+            <div className={styles.aboutSection__container}>
+                <div className={styles.aboutSection__container__line}></div>
+                <div className={styles.aboutSection__container__paragraph}>
+                    <p>"We navigate the digital cosmos to bring your brand to life with precision, creativity, and cutting-edge technology.</p>
+                    <p>Whether refining your personal brand, amplifying your online presence, or crafting stunning graphics and websites — we deliver comprehensive digital communication solutions tailored to your vision."</p>
+                </div>
             </div>
         </header>
     )
