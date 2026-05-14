38abeeee6f0edfab3f0102bdc3663249e72e20d3
Lebo Madikane <madikanelebohang@gmail.com>
2026-05-14 21:14:11 +0200

    build Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,30 @@
 import Link from 'next/link'
 import styles from './Footer.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faPlaneDeparture } from "@fortawesome/free-solid-svg-icons";
+import { faInstagram, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import Image from 'next/image';
 
 export default function Footer() {
 
     return (
         <header className={styles.footer}>
             <div className={styles.container}>
-
+                <div className={styles.footerLinks}>
+                    <div className={styles.footerLinksOne}>
+                        <Link href={''}><FontAwesomeIcon icon={faEnvelope} className={styles.icons}></FontAwesomeIcon></Link>
+                        <Link href={''}><FontAwesomeIcon icon={faLinkedinIn} className={styles.icons}></FontAwesomeIcon></Link>
+                        <Link href={''}><FontAwesomeIcon icon={faInstagram} className={styles.icons}></FontAwesomeIcon></Link>
+                    </div>
+                    <div className={styles.footerLinksTwo}>
+                        <Link href="/#about" className={styles.footerLink}>About</Link>
+                        <Link href="/services" className={styles.footerLink}>Services</Link>
+                        <Link href="/#contact" className={styles.footerLink}>Contact</Link>
+                    </div>
+                </div>
+                <div className={styles.footerAtum}>
+                    <Image className={styles.footerImage} src={'/footer-images/AtumFooter.svg'} alt="Footer Image" width={150} height={100} priority={true} />
+                </div>
             </div>
         </header>
     )
