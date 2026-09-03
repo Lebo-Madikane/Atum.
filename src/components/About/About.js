b6c15f44101c18debbfe53fad26d06feb4f058e9
Lebo Madikane <madikanelebohang@gmail.com>
2026-09-03 11:20:38 +0200

    update text content

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -17,7 +17,7 @@ export default function About() {
                 <h2 className={styles.servicesText}>About</h2>
                 <div className={styles.aboutSection__container__paragraph}>
                     <p className={styles.noToMobile}>"We navigate the digital cosmos to bring your brand to life with precision, creativity, and cutting-edge technology.</p>
-                    <p>Whether refining your personal brand, amplifying your online presence, or crafting stunning graphics and websites — we deliver comprehensive digital solutions tailored to your vision."</p>
+                    <p>"Whether refining your personal brand, amplifying your online presence, or crafting stunning graphics and websites — we deliver comprehensive digital solutions tailored to your vision."</p>
                 </div>
             </div>
         </motion.section>
